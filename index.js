40c6e5ecd357a462d710d54f458516cdb45a16b5
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-04-04 18:44:11 +0300

    algorithm lab

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
-
+    let RightToLeft =""
+    for (let i= word.length-1; i>=0;i--){
+      RightToLeft += word[i]
+    }
+    
+    return  word===RightToLeft?true:false
+  
+    }
+  isPalindrome("lot")
 /* 
   Add your pseudocode here
+    iterate through argument and store value with text starting from right to left
+    introduce a conditional statement for comparison
+    call function with a string argument
+
 */
 
 /*
   Add written explanation of your solution here
-*/
+  When function is called with 'lot' argument 
+  line 3 declares and initializes with an empty string
+  the for loop iterates over the word parameter('lot') and for each iteration;
+    takes the argument as an array and then beginning at the end of the array;
+      iteration 1: length is at 3 and index 2 (length-1)=> saves 't'
+      iteration 2:                    index 1 => saves 'o'
+      iteration 3:                    index 0 => saves 'l'
+    with each iteration, the variable Right to left takes an increment of the element
+    currently . We then end up with "tol" when i is < 0 and the iteration has reached an end
+  word parameter is compared with the new reversed string and returns true or false if the 
+  letters (strings) match or dont respectively
+*/ 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
